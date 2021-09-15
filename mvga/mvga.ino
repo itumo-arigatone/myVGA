aa3165b1c03b1437efecba207a987d933e80db99
unkomorasi01 <soganokasegi0825@gmail.com>
2021-09-16 01:28:33 +0900

    調査用にアセンブリを変更

---
+++ mvga/mvga.ino
@@ -0,0 +1,272 @@
+// 1pxの色を2bitで表現している つまり一回の通信で4px分のデータを送信することができる。
+// 1pxの色を3bitで表現するようにすればいいってことだよなぁ！？これ、1.5倍メモリを消費するってことか
+// 白111　赤100 緑 010 青 001
+// 前か後の2bitは捨てるしかないかな？ 2pxしか送れない 00100111 このデータを送ったとして赤白って2pxのデータが渡せる。
+#define HSYNCPIN 3
+#define VSYNCPIN 9
+#define COLORRED 6
+#define COLORGREEN 7
+#define COLORBLUE 5
+#define VGAX_HEIGHT 60
+#define VGAX_BWIDTH 30
+#define VGAX_WIDTH (VGAX_BWIDTH*4) //number of pixels in a row
+#define VGAX_BSIZE (VGAX_BWIDTH*VGAX_HEIGHT) //size of framebuffer in bytes
+#define VGAX_SIZE (VGAX_WIDTH*VGAX_HEIGHT) //size of framebuffer in pixels
+#define IMG_PIRATE_BWIDTH 30
+#define IMG_PIRATE_HEIGHT 60
+#define SKIPLINES 90
+
+const unsigned char img_pirate_data[IMG_PIRATE_HEIGHT*IMG_PIRATE_BWIDTH] PROGMEM={
+  64, 63,63,63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,
+  128, 63,63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,63, 63, 63, 63, 63, 63,
+  32,63,63, 63,63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,
+  16,63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,
+ 63,63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,
+ 63, 63,63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,
+ 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,63, 63, 63, 63, 63, 63, 63,
+  63,63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,
+  63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,
+  63,63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,
+  63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,
+  63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,
+  63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,63, 63, 63, 63, 63, 63, 63, 63, 63,
+  63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,
+  63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,63,63,63,63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,
+  63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,63, 63, 63, 63,63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,
+  63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,63, 63,63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,
+  63, 63, 63, 63, 63, 63, 63, 63,63,63, 63, 63,63,63, 63, 63,63,63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,
+  63, 63, 63, 63, 63, 63, 63,63, 63, 63,63,63,63,63,63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,
+  63, 63, 63, 63, 63, 63, 63,63,63,63,63,63,63,63,63,63,63,63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,
+  63, 63, 63, 63, 63, 63, 63, 63,63,63,63,63,63,63,63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,
+  63, 63, 63, 63, 63, 63, 63,63,63,63,63,63,63,63,63, 63, 63,63,63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,
+ 63, 63, 63, 63, 63, 63, 63,63,63,63, 63, 63,63,63,63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,
+  63,63, 63, 63, 63, 63, 63,63,63,63,63, 63,63,63,63, 63, 63, 63,63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,
+  63, 63,63, 63, 63, 63, 63,63,63,63,63, 63,63,63,63,63,63,63, 63,63, 63,63, 63, 63, 63, 63, 63, 63, 63, 63,
+  63, 63, 63, 63, 63, 63,63,63,63,63, 63, 63,63,63,63,63, 63,63,63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,
+  63, 63, 63, 63, 63, 63, 63,63,63,63,63,63,63,63,63, 63,63,63,63,63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,
+  63, 63, 63, 63, 63, 63,63,63,63,63,63,63,63,63,63, 63,63,63,63,63, 63,63, 63, 63, 63, 63, 63, 63, 63, 63,
+  63, 63, 63, 63, 63, 63,63,63,63,63,63,63,63,63,63,63,63,63,63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,
+  63, 63, 63, 63, 63, 63, 63,63,63,63,63,63,63,63,63,63,63,63,63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,
+  63, 63, 63, 63, 63, 63, 63,63,63,63,63,63,63,63,63,63,63,63,63,63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,
+  63, 63, 63, 63, 63, 63,63,63,63,63,63,63,63,63,63,63, 63,63,63, 63, 63,63, 63, 63, 63, 63, 63, 63, 63, 63,
+  63, 63, 63, 63, 63, 63, 63,63,63,63,63,63,63,63,63,63,63,63,63,63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,
+  63, 63, 63,63, 63, 63,63,63,63,63,63,63,63,63,63,63, 63,63,63,63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,
+  63, 63, 63, 63, 63, 63,63,63,63,63,63,63,63,63,63,63,63,63,63,63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,
+  63, 63, 63, 63, 63, 63, 63,63,63,63,63,63,63,63,63,63, 63, 63,63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,
+  63, 63, 63, 63, 63, 63, 63,63,63,63,63,63,63,63, 63,63, 63, 63,63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,
+  63, 63, 63, 63, 63, 63, 63,63,63,63,63,63,63, 63, 63, 63, 63, 63, 63, 63, 63, 63,63,63, 63, 63, 63, 63, 63, 63,
+  63, 63, 63, 63, 63, 63, 63, 63,63,63,63,63,63, 63, 63, 63, 63, 63,63, 63, 63, 63,63,63,63, 63, 63, 63, 63, 63,
+  63, 63,63, 63, 63, 63, 63, 63,63,63,63,63, 63, 63, 63,63,63, 63, 63, 63, 63,63, 63, 63,63, 63, 63, 63, 63, 63,
+  63, 63,63, 63,63, 63, 63,63,63,63,63,63, 63,63,63,63,63,63, 63, 63, 63, 63, 63,63,63,63, 63, 63, 63, 63,
+  63,63,63, 63, 63, 63, 63, 63, 63,63,63,63, 63,63,63,63,63, 63, 63, 63, 63, 63, 63,63,63,63, 63, 63, 63, 63,
+  63,63, 63, 63, 63, 63, 63, 63, 63,63,63, 63,63,63,63,63,63,63, 63, 63, 63, 63, 63,63,63,63, 63, 63, 63, 63,
+ 63,63, 63,63,63, 63, 63, 63, 63,63,63, 63,63,63,63,63,63, 63, 63, 63, 63, 63, 63,63,63,63, 63, 63, 63, 63,
+ 63,63,63,63, 63, 63, 63, 63, 63, 63,63, 63,63,63,63,63,63,63, 63, 63, 63, 63,63,63,63,63, 63, 63, 63, 63,
+ 63,63,63,63, 63, 63, 63, 63, 63, 63, 63, 63,63,63,63,63,63,63, 63, 63, 63, 63,63, 63,63,63,63, 63, 63, 63,
+ 63,63,63,63, 63,63, 63, 63, 63, 63, 63, 63,63,63,63,63,63,63, 63, 63, 63, 63, 63, 63,63,63,63, 63, 63, 63,
+ 63,63,63, 63, 63,63, 63, 63, 63, 63, 63, 63,63,63,63,63,63, 63, 63, 63, 63, 63, 63,63,63,63,63, 63, 63, 63,
+ 63,63,63,63, 63,63,63, 63, 63, 63, 63, 63,63,63,63,63,63, 63, 63, 63, 63, 63, 63, 63,63,63,63, 63, 63, 63,
+ 63,63, 63,63,63, 63, 63, 63, 63, 63, 63, 63, 63,63,63, 63,63, 63, 63, 63, 63, 63, 63, 63,63,63,63,63, 63, 63,
+ 63,63, 63,63,63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,63,63,63,63,63, 63,
+ 63,63,63,63,63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,63,63,63,63,63, 63,
+ 63,63,63,63,63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,63, 63, 63,63,63,63,63,63, 63,
+ 63,63,63,63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,63, 63, 63,63,63,63,63,63,63, 63,
+ 63,63,63,63, 63,63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,63,63,63,63,63,63,63,
+ 63,63,63,63,63,63,63,63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63, 63,63,63, 63,63,63,63,63,63,63,63,
+ 63,63,63,63,63,63,63, 63,63, 63, 63, 63, 63, 63, 63, 63, 63, 63,63, 63, 63,63, 63,63,63,63,63,63,63,63,
+ 63,63,63,63,63,63,63,63,63, 63, 63, 63, 63, 63, 63, 63, 63, 63,63, 63, 63,63, 63,63,63,63,63,63,63,63,
+ 63,63,63,63,63,63,63,63,63, 63, 63, 63, 63,63, 63, 63, 63, 63,63, 63, 63,63, 63,63,63,63,63,63,63,63,
+ 63,63,63,63,63,63,63, 63,63,63, 63, 63, 63,63,63, 63, 63, 63,63,63, 63,63,63,63,63,63,63,63,63,63,
+};
+
+static byte afreq, afreq0;
+unsigned vtimer;
+static byte aline, rlinecnt;
+static byte vskip;
+byte vgaxfb[VGAX_HEIGHT*VGAX_BWIDTH];
+
+//VSYNC interrupt
+ISR(TIMER1_OVF_vect) {
+  aline=-1;
+  vskip=SKIPLINES;
+  vtimer++;
+  rlinecnt=0;
+}
+//HSYNC interrupt
+ISR(TIMER2_OVF_vect) {
+  /*
+  NOTE: I prefer to generate the line here, inside the interrupt.
+  Gammon's code generate the line pixels inside main().
+  My versin generate the signal using only interrupts, so inside main() function
+  you can do anything you want. Your code will be interrupted when VGA signal
+  needs to be generated
+  */
+  //generate audio modulation. around 15 clocks
+  asm volatile(                                   //4c to load Z and Y
+    "      ld r16, z                        \n\t" //c1 r16=afreq
+    "      cpi %[freq0], 0                  \n\t" //c1 afreq0==0 ?
+    "play_audio:                            \n\t" 
+    "      cpi r16, 0                       \n\t" //c1 afreq==0 ?
+    "      brne dont_flip_audio_pin         \n\t" //c1/2 *1
+    "flip_audio_pin:                        \n\t" 
+    "      ldi r18, 1                       \n\t" //c1
+    "      out %[audiopin], r18             \n\t" //c1
+    "      st Z, %[freq0]                   \n\t" //c1 afreq=afreq0
+    "      rjmp end                         \n\t" //c2
+    "dont_flip_audio_pin:                   \n\t" 
+   "      dec r16                          \n\t" //c1
+    "      st z, r16                        \n\t" //c1
+    "end:                                   \n\t"
+  :
+  : "z" (&afreq),
+    [freq0] "r" (afreq0),
+    [audiopin] "i" _SFR_IO_ADDR(PINC)
+  : "r16", "r18");
+
+  //check vertical porch
+  if (vskip) {
+      vskip--;
+      return;
+  }
+  if (rlinecnt<VGAX_HEIGHT) {   
+    //interrupt jitter fix (needed to keep signal stable)
+    //code from https://github.com/cnlohr/avrcraft/tree/master/terminal
+    //modified from 4 nop align to 8 nop align
+    // Zはメモリの番地指定に使用できる。
+    #define DEJITTER_OFFSET 1
+    #define DEJITTER_SYNC -2
+    asm volatile(
+      "     st Z, r16             \n\t" // レジスタからアドレスへの代入 z　Pointer register pair Z　z (r31:r30)
+      "     lds r16, %[timer0]    \n\t" //データ転送　１バイトの拡張レジスタから汎用レジスタへ転送する。
+      "     subi r16, %[tsync]    \n\t" // 汎用レジスタにrとM(定数)おいて引き算する。定数は0から255になる。
+      "     andi r16, 7           \n\t" // レジスタと定数の論理積
+      "     call TL               \n\t" // 絶対アドレス TLを指定
+      "TL:                        \n\t" //
+      "     pop r31               \n\t" //1バイトの標準I/Oレジスタの内容を汎用レジスタに転送する。スタックから Rd に復帰
+      "     pop r30               \n\t" //
+      "     adiw r30, (LW-TL-5)   \n\t" //
+      "     add r30, r16          \n\t" // レジスタ間加算
+      "   adc r31, __zero_reg__ \n\t" //
+      "     ijmp                  \n\t" //
+      "LW:                        \n\t" //
+      "     nop                   \n\t" //
+      "     nop                   \n\t" //
+      "     nop                   \n\t" //
+      "     nop                   \n\t" //
+      "     nop                   \n\t" //
+      "     nop                   \n\t" //
+      "     nop                   \n\t" //
+      //"   nop                   \n\t" //
+      "LBEND:                     \n\t" //
+    :
+    : [timer0] "i" (&TCNT0),
+      [toffset] "i" ((uint8_t)DEJITTER_OFFSET),
+      [tsync] "i" ((uint8_t)DEJITTER_SYNC)
+    : "r30", "r31", "r16", "r17");
+    /*
+    Output all pixels.
+
+    NOTE: My trick here is to unpack 4 pixels and shift them before writing to
+    PORTD.
+
+    Pixels are packed as 0b11223344 because the first pixel write have no time
+    to perform a shift (ld, out) and must be prealigned to the two upper bits 
+    of PORTD, where the two wires of the VGA DSUB are connected. The second, 
+    the third and the forth pixels are shifted left using mul opcode instead 
+    of a left shift opcode. Shift opcodes are slow and can shift only 1 bit at
+    a time, using 1 clock cycle. mul is faster.
+
+    Instead of using a loop i use the .rept assembler directive to generate an 
+    unrolled loop of 30 iterations.
+    */
+    // r20とかには1byte入る
+    // r0はr16に連動している
+    //64で16赤,0緑
+    asm volatile (
+      "    ldi r20, 2       \n\t" //const for <<2bit r20に定数4を転送 
+      //".rept 60             \n\t" //output 4 pixels for each iteration 30*4=120
+      "    ld r16, Z+       \n\t" // ld:１バイトのデータメモリを汎用レジスタに転送。 
+                                  // Zはメモリの番地を指定している？
+                                  // 事後増加付きZﾚｼﾞｽﾀ間接での取得 	Rd ← (Z), Z ← Z + 1
+      "    out %[port], r16 \n\t" //write pixel 1 out I,r 1バイトの汎用レジスタの内容を標準I/Oレジスタに転送する。
+      "    mul r16, r20     \n\t" //<<2 汎用レジスタ間でに乗算させる。r16*r20ってこと？　r16*4 次のループで4px進めないと上書きしちゃうからだ！！
+      "    out %[port], r0  \n\t" //write pixel 2
+      //"    mul r0, r20      \n\t" //<<4 r0*4
+      //"    out %[port], r0  \n\t" //write pixel 3
+      //"    mul r0, r20      \n\t" //<<6
+      //"    out %[port], r0  \n\t" //write pixel 4
+      //".endr                \n\t" //
+      "    nop              \n\t" //expand last pixel
+      "    ldi r16, 0       \n\t" //
+      "    out %[port], r16 \n\t" //write black for next pixels
+    :
+    : [port] "I" (_SFR_IO_ADDR(PORTD)), // ポートD (デジタルピン0から7) // 入力オペランド　[マクロ名2] "制約" (変数名2)
+      "z" "I" (/*rline*/(byte*)vgaxfb + rlinecnt*VGAX_BWIDTH) // vgaxfb[VGAX_HEIGHT(60)*VGAX_BWIDTH(30)], rlinecnt*VGAX_BWIDTH 
+    : "r16", "r17", "r20", "r21", "memory");
+    // VGAX_BWIDTH=30
+    // rlinecntは0から増えていく？
+    //Serial.println(_SFR_IO_ADDR(PORTD));
+
+    //increment framebuffer line counter after 6 VGA lines
+    #if defined(__AVR_ATmega2560__) && defined(ATMEGA2560_MAXRES)
+      #define CLONED_LINES (2-1)
+    #else
+      #define CLONED_LINES (6-1)
+    #endif
+    if (++aline==CLONED_LINES) { 
+      aline=-1;
+      rlinecnt++;
+    } else {
+    }
+  } 
+}
+
+
+void begin() {
+  cli();
+    //disable TIMER0 interrupt
+  TIMSK0=0;
+  TCCR0A=0;
+  TCCR0B=(1 << CS00); //enable 16MHz counter (used to fix the HSYNC interrupt jitter)
+  OCR0A=0;
+  OCR0B=0;
+  TCNT0=0;
+  //TIMER1 - vertical sync pulses
+  pinMode(VSYNCPIN, OUTPUT);
+  TCCR1A=bit(WGM11) | bit(COM1A1);
+  TCCR1B=bit(WGM12) | bit(WGM13) | bit(CS12) | bit(CS10); //1024 prescaler
+  ICR1=259; //16666 / 64 uS=260 (less one)
+  OCR1A=0; //64 / 64 uS=1 (less one)
+  TIFR1=bit(TOV1); //clear overflow flag
+  TIMSK1=bit(TOIE1); //interrupt on overflow on TIMER1
+  //TIMER2 - horizontal sync pulses
+  pinMode(HSYNCPIN, OUTPUT);
+  TCCR2A=bit(WGM20) | bit(WGM21) | bit(COM2B1); //pin3=COM2B1
+  TCCR2B=bit(WGM22) | bit(CS21); //8 prescaler
+  OCR2A=63; //32 / 0.5 uS=64 (less one)
+  OCR2B=7; //4 / 0.5 uS=8 (less one)
+  TIFR2=bit(TOV2); //clear overflow flag
+  TIMSK2=bit(TOIE2); //interrupt on overflow on TIMER2
+  pinMode(COLORRED, OUTPUT);
+  pinMode(COLORGREEN, OUTPUT);
+  pinMode(COLORBLUE, OUTPUT);
+  sei();
+}
+
+void copy(byte *src) {
+  byte *o=(byte*)vgaxfb;
+  unsigned cnt=VGAX_BSIZE;
+  while (cnt--)
+    *o++=pgm_read_byte(src++);
+}
+
+void setup() {
+  Serial.begin(9600);
+  begin();
+  copy((byte*)img_pirate_data);
+}
+
+void loop() {
+  static unsigned cnt;
+  copy((byte*)img_pirate_data);
+}
